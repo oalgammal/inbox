6e26cde7da00bd80f0078c976179b64626256488
oalgammal <oalgammal@gmail.com>
2022-11-22 23:56:29 +0200

    working

---
+++ contracts/Inbox.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Inbox {
+    string public  message;
+   
+    function A (string memory intialMessage) public{
+        message = intialMessage;
+    }
+    function setMessage(string memory newMessage)public{
+        message = newMessage;
+    }
+  
+}
