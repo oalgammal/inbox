8507ec02d4467152019334a8444740550171c42f
oalgammal <oalgammal@gmail.com>
2022-11-23 17:45:01 +0200

    finish

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -4,9 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Inbox {
     string public  message;
    
-    constructor (string memory intialMessage) public{
-        message = intialMessage;
-    }
+  
     function setMessage(string memory newMessage)public{
         message = newMessage;
     }
